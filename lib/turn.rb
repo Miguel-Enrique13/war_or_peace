69be20d1c690dd5753a355f5ab2973796edf29a0
Miguel Guardia <guardia@ualberta.ca>
2020-07-02 14:35:42 -0700

    Add Iteration 2

---
+++ lib/turn.rb
@@ -0,0 +1,68 @@
+require './lib/deck'
+require './lib/card'
+require './lib/player'
+require 'pry'
+
+class Turn
+
+  attr_reader :player1, :player2, :spoils_of_war
+  def initialize (player1, player2)
+    @player1 = player1
+    @player2 = player2
+    @spoils_of_war = []
+  end
+
+  def type
+    if @player1.player_deck.cards[0].rank > @player2.player_deck.cards[0].rank || @player1.player_deck.cards[0].rank < @player2.player_deck.cards[0].rank
+      :basic
+    elsif @player1.player_deck.cards[2].rank > @player2.player_deck.cards[2].rank || @player1.player_deck.cards[2].rank < @player2.player_deck.cards[2].rank
+      :war
+    else
+      :mutually_assured_destruction
+    end
+  end
+
+  def winner
+    if type == :basic
+      if @player1.player_deck.rank_of_card_at(0) >  @player2.player_deck.rank_of_card_at(0)
+        @player1
+      else
+        @player2
+      end
+    elsif type == :war
+      if @player1.player_deck.rank_of_card_at(2) >  @player2.player_deck.rank_of_card_at(2)
+        @player1
+      else
+        @player2
+      end
+    else
+      "No Winner"
+    end
+
+  end
+
+  def pile_cards
+    if type == :basic
+      @spoils_of_war << @player1.player_deck.remove_card
+      @spoils_of_war << @player2.player_deck.remove_card
+      return @spoils_of_war
+    elsif type == :war
+      3.times do
+        @spoils_of_war << @player1.player_deck.remove_card
+        @spoils_of_war << @player2.player_deck.remove_card
+        end
+      return @spoils_of_war
+    else
+      3.times do
+        @player1.player_deck.remove_card
+        @player2.player_deck.remove_card
+      end
+      return @spoils_of_war
+    end
+  end
+
+  def award_spoils(winner)
+    (winner.player_deck.cards << @spoils_of_war).flatten!
+    @spoils_of_war = []
+  end
+end
